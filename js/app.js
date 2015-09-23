a1669c0404274377b84c19994fa65ded52fe1284
Martin Laine <martin.laine@simplybusiness.co.uk>
2015-09-23 17:14:42 +0100

    Site updated: 2015-09-23 17:14:41

---
+++ js/app.js
@@ -0,0 +1,25 @@
+"use strict";
+
+(function () {
+
+  var commentBox = document.getElementById("js-comment-box");
+
+  if (commentBox) {
+    var loadScript = function(src) {
+      var s = document.createElement("script");
+      s.async = true;
+      s.src = src;
+      (document.getElementsByTagName("head")[0]).appendChild(s);
+    }
+
+    loadScript("//" + disqus_shortname + ".disqus.com/count.js");
+
+    document.getElementById("js-toggle-comments").addEventListener("click", function (e) {
+      e.preventDefault();
+      commentBox.focus();
+      commentBox.classList.add("o-comment-box--is-open");
+      loadScript("//" + disqus_shortname + ".disqus.com/embed.js");
+    }, false);
+  }
+
+})();
